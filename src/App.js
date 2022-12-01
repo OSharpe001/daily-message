3286e2689c947c4383caba5ef110250bf93d0f4a
O. Sharpe <osharpe001@gmail.com>
2022-12-01 13:16:08 -0500

    more conditional rendering practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const time = new Date();
+  const day = time.toLocaleString("en-us", { weekday: "long" });
+  const morning = time.getHours() >= 6 && time.getHours() <=11;
+  let dayMessage;
+
+  if (day.toLowerCase() === "monday") {
+    dayMessage = `Booooo! It's ${day}`;
+  } else if (day.toLowerCase() === "tuesday") {
+    dayMessage = `${day}, four days to go...`;
+  } else if (day.toLowerCase() === "wednesday") {
+    dayMessage = `It's ${day}. We're half way there!`;
+  } else if (day.toLowerCase() === "thursday") {
+    dayMessage = `${day}, time to start planning out 
+    this weekend!`;
+  } else if (day.toLowerCase() === "friday") {
+    dayMessage = `${day}!!! Let's get through it and 
+    start this weekend!`;
+  } else if (day.toLowerCase() === "saturday") {
+    dayMessage = `It's ${day}! 24 hours of "play-time"!`;
+  } else {
+    dayMessage = `Oh, boy... ${day}'s here, y'all. Get 
+    the last of your fun in and get some rest. We'll 
+    have to do this all over again, tomorrow.`
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{dayMessage}</h1>
+      {morning ? <h2>Did you get to eat breakfast, yet?</h2>: ""}
     </div>
   );
-}
+};
 
 export default App;
